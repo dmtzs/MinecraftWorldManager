b649f79f5c8a86290125ab11d289ad2b7ac18d91
Diego Ms <diego.mtzs00@gmail.com>
2021-08-22 13:35:17 -0500

    Update for Maincra python file
    Separation of some of the messages that appears everytime that something happen in the program. Now are called just the method from the local library

--- Maincra.py
+++ Maincra.py
@@ -1,6 +1,5 @@
 try:
     import os
-    import time
     from Mets import metodos
 except ImportError as errorimp:
     print(f"Ocurrió el siguiente error de importación: {errorimp}")
@@ -16,24 +15,35 @@ def main(accion, comm, pants):
         op= input("Ingresa la opción deseada: ")
         paso= accion.ValidarInt(op)
         if paso:
+            op= int(op)
             if op== 1:
                 resuls= os.walk(rutaOrigen)
 
                 archivosMaincra= accion.ObtenerArchivos(resuls)
                 accion.Guardar(rutaOrigen, rutaGuardar, archivosMaincra)
+                pants.MensajeFinalOpciones(comm, 0)
             elif op== 2:
+                os.system(comm)
                 resuls= os.walk(rutaGuardar)
                 archivosMaincra= accion.ObtenerArchivos(resuls)
-                for i in archivosMaincra:
-                    print(i)
-                #Falta la parte de seleccionar el archivo y pasarlo a downloads
+                if len(archivosMaincra) > 0:
+                    for i in range(len(archivosMaincra)):
+                        print(f"\n\t\t{i+1}. {archivosMaincra[i]}")
+                    op2= input("Ingresa el número correspondiente al número del mundo que deseas pasar a descargas: ")
+                    paso2= accion.ValidarInt(op2)
+                    if paso2:
+                        op2= int(op2)
+                        accion.Descargas(rutaGuardar, rutaOrigen, archivosMaincra[op2-1])
+                        pants.MensajeFinalOpciones(comm, 1)
+                    else:
+                        pants.SoloEnteros(comm)
+                else:
+                    pants.NoArchivos(comm)
 
             elif op== 3:
                 salir= False
         else:
-            os.system(comm)
-            print("Solo están permitidos números enteros")
-            time.sleep(3)
+            pants.SoloEnteros(comm)
 
 if __name__== "__main__":
     try:
@@ -43,11 +53,13 @@ if __name__== "__main__":
         comm= accion.LimpiarConsolaSO()
         
         main(accion, comm, pants)
-        print("\n\n\n\t\t\tMundos de Minecraft movidos con éxito!!!")
+
     except Exception as e:
         os.system(accion.LimpiarConsolaSO())
         print(f"Ocurrió el siguiente error: {e}")
+
     except KeyboardInterrupt:
         print("Se presiono Ctrl+c terminando programa")
+
     finally:
-        print("Programa finalizado")
+        print("Finalizando programa")
